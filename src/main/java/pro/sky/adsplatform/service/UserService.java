e682835843f1bfac4dd6ca0d360c93d2f8865048
L <EBogatyrevaPK@yandex.ru>
2022-11-05 13:08:16 +1200

    1. Добавлены изменения в UserService))

--- src/main/java/pro/sky/adsplatform/service/UserService.java
+++ src/main/java/pro/sky/adsplatform/service/UserService.java
@@ -3,6 +3,7 @@ package pro.sky.adsplatform.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import pro.sky.adsplatform.dto.NewPasswordDto;
 import pro.sky.adsplatform.entity.UserEntity;
 import pro.sky.adsplatform.repository.UserRepository;
 
@@ -39,4 +40,41 @@ public class UserService {
     public List<UserEntity> getAllUsers() {
         return userRepository.findAll();
     }
+
+    /**
+     *Обновление данные пользователя.
+     * @return
+     */
+    public UserEntity updateUser(UserEntity userEntity) {
+        LOGGER.info("Обновление данных пользователя");
+/*        if (userEntity.getId() != null) {
+            userRepository.findById(userEntity.getId())
+                            .ifPresent(user -> {
+                                user.setFirstName(userEntity.getFirstName());
+                                user.setLastName(userEntity.getLastName());
+                                user.setPhone(userEntity.getPhone());
+                                user.setId(userEntity.getId());
+                                user.setEmail(userEntity.getEmail());
+                            });
+           }*/
+        return userRepository.save(userEntity);
+    }
+
+    /**
+     *Обновление пароля пользователя.
+     * @return
+     */
+    public UserEntity setPassword(UserEntity newPasswordDto) {
+        LOGGER.info("Обновление пароля пользователя");
+/*                if (newPasswordDto.getId() != null) {
+            userRepository.findById(newPasswordDto.getId())
+                            .ifPresent(user -> {
+                                user.setPassword(newPasswordDto.getPassword());
+                            });
+           }*/
+        return userRepository.save(newPasswordDto);
+    }
+
+
+
 }
